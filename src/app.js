12c21bc1e8f40eaa09e9a9b1d696a1ee5993f95c
Marie <marievstewart@gmail.com>
2023-02-15 17:28:55 +0000

    First Javascript project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Whatup from the console!");
 };
+
+function generateExcuse() {
+  let pronoun = ["A", "The"];
+  let subject = ["racoon", "kiwi", "dog", "panther", "tortilla"];
+  let action = ["ran over", "bit", "slapped", "threw", "defenestrated"];
+  let possesion = ["apples", "coconuts", "crayons", "mice"];
+  let where = ["on the street", "in the temple", "in the boat", "at Sedanos"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possesion[possesionIndex] +
+    " " +
+    where[whereIndex]
+  );
+}
